50681f0ddb4c043a0e680fcaddb9294956e2e01f
eishi1103 <78992733+eishi1103@users.noreply.github.com>
2023-02-23 17:20:04 +0900

    ファイルを修正してみた

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        int i = 10;
+        if (i < 5) {
+            System.out.println("iは５以上です");
+        } else {
+            System.out.println("iは5未満です");
+        }
+
+        System.out.println("Githubに変更を反映死体！");
     }
 }
